a59ad7071ff87f7c43ef3c19744a333f43a450cb
Martín <magadiflo@gmail.com>
2022-12-20 15:16:17 -0500

    Property Binding

--- src/app/otros/data-binding/data-binding.component.ts
+++ src/app/otros/data-binding/data-binding.component.ts
@@ -8,10 +8,14 @@ import { Component, OnInit } from '@angular/core';
 export class DataBindingComponent implements OnInit {
 
   nombre: string = 'Martín';
+  textoPlaceholder: string = 'Escriba algo aquí';
+  deshabilitado: boolean = true;
+  imgSrc: string = 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Angular_full_color_logo.svg/2048px-Angular_full_color_logo.svg.png';
 
   constructor() {
     setInterval(() => {
       this.nombre = 'Gaspar';
+      this.deshabilitado = false;
     }, 3000);
   }
 
