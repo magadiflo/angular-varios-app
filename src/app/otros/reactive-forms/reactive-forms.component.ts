64e5e6332335fbe4dae7bc4bd7c8dba749b02b66
Martín <magadiflo@gmail.com>
2022-12-23 17:24:02 -0500

    Form Control y Control Value Accessor en Angular - Implementación de componente de angular como FormControl personalizado

--- src/app/otros/reactive-forms/reactive-forms.component.ts
+++ src/app/otros/reactive-forms/reactive-forms.component.ts
@@ -8,13 +8,43 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 })
 export class ReactiveFormsComponent {
 
+  /**
+   * * Como parte de las necesidades que tenemos es que queremos tener un campo en el formulario, es decir queremos
+   * * tener un nuevo FormControl llamado rating. Ahora, queremos que ese campo sea el componente de Angular que hemos
+   * * creado (<app-star-rating></app-star-rating>) y para ello hacemos como normalmente hacemos con los otros 
+   * * componentes nativos de formulario (input, select, etc..), es decir le agregamos el formControlName="rating", pero 
+   * * eso no sería todo para que empiece a furncional, ya que por el momento, solo haciendo eso nos mostrará un error
+   * * en consola: ERROR Error: No value accessor for form control with name: 'rating'.
+   * * Ese error nos quiere decir que no encuentra un Accessor para el componente que estamos estableciéndole como 
+   * * FormControl ya que de por sí es un componente y no un elemento nativo de formulario (input, select, textarea, etc..).
+   * * Entonces, para que nuestro componente sea reconocido dentro del Formulario Reactivo, debe tener su propio intermediario,
+   * * entre el FormControl y ese componente, es decir, debe tener su propio ControlValueAccessor.
+   * *
+   * * 1. Y eso se debe hacer directamente en el componente que se quiere controlar, es decir, en el componente StarRatingComponent.ts
+   * *    debemos implementar la interfaz ControlValueAccessor e implementar sus 3 métodos (aunque el 4to es opcional).
+   * * 2. Debemos utilizar un providers dentro de dicho componente (con eso ya estamos creando ese puente, ese intermediario)
+   * *    providers: [
+    **      {
+    **        provide: NG_VALUE_ACCESSOR,
+    **        useExisting: StarRatingComponent,
+    **        multi: true //* Por si queremos utilizar más proveedores
+    **      }
+    **    ]
+    ** 3. Ahora, nosotros teneoms que decirle a nuestro puente, nuestro intermediario,
+    **    es decir a nuestro ControlValueAccessor implementado, cómo es que se debe comportar,
+    **    para que pueda enviar información hacia el FormControl y de esa manera, desde esde
+    **    ReactiveFormsComponent miFormulario, podamos acceder a sus valores
+   */
+
   miFormulario: FormGroup = new FormGroup({
     title: new FormControl('El señor de los anillos', [Validators.required]),
+    rating: new FormControl(3, [Validators.min(3), Validators.max(5)]),
   });
 
   guardar(): void {
+    console.log('form valid? ', this.miFormulario.valid);   
     console.log(this.miFormulario.value);
-    console.log(this.miFormulario.valid);
+    console.log('rating touched? ', this.miFormulario.controls['rating'].touched);
   }
 
 }
--- src/app/otros/reactive-forms/star-rating/star-rating.component.ts
+++ src/app/otros/reactive-forms/star-rating/star-rating.component.ts
@@ -1,14 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, forwardRef } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
+
+/**
+ * * forwardRef(), permite referirse a referencias que aún no están definidas. 
+ * * Por ejemplo, forwardRef se usa cuando se declara el token al que debemos hacer
+ * * referencia para fines DI, pero aún no se ha defindo. También se usa cuando el token que
+ * * usamos al crear una consulta aún no está definido.
+ * *
+ * * RESUMEN: Si nosotros agregamos un proveedor, como en este caso el NG_VALUE_ACCESSOR,  
+ * * para que lo use con nuestro componente StarRatingComponent y no lo reconoce, es decir
+ * * nos sale error, es que posiblemente esté descargando ese componente de manera lazy, 
+ * * cuando eso sucede, quiere decir que la instancia de ese componente (StarRatingComponent)
+ * * aún no está lista. En ese sentido, es que usamos la función forwardRef(...) para decirle que
+ * * coga la instancia de nuestro componente StarRatingComponent y espere hasta que esté disponible,
+ * * de esa manera nos aseguramos que usaremos la instancia correcta en el momento correcto
+ */
 
 @Component({
   selector: 'app-star-rating',
   templateUrl: './star-rating.component.html',
-  styleUrls: ['./star-rating.component.css']
+  styleUrls: ['./star-rating.component.css'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => StarRatingComponent), //* forwardRef(), nos permite
+      multi: true //* Por si queremos utilizar más proveedores
+    }
+  ]
 })
-export class StarRatingComponent implements OnInit {
+export class StarRatingComponent implements ControlValueAccessor {
 
-  private _rating: number = 3;
+  private _rating: number = 1;
   ratingArray: number[] = [0, 1, 2, 3, 4];
+
+  //* La función que va a manipular el ControlValueAccessor
+  private _onChanged: Function = (_value: number) => { };
+  private _onTouch: Function = (_value: number) => { };
 
 
   ngOnInit(): void {
@@ -21,8 +48,33 @@ export class StarRatingComponent implements OnInit {
 
   onClick(rating: number): void {
     this._rating = rating;
-    console.log(this._rating);
+    this._onChanged(this._rating); //* Desencadenando función cuando hay cambios
+    this._onTouch(this._rating); //* Desencadenamos la función cuando se ha tocado el control pasándole el valor
   }
 
+  //*#region  ControlValueAccesso
+
+  //***** Para ver qué hacen estos métodos revisar la teoría dejada en el html ¿Qué es el ControlValueAccessor (CVA)? ******** */
+
+  //* Aquí se pasa el valor que esté interactuando con miFormulario.rating
+  writeValue(value: number): void {
+    if (value) {
+      this._rating = value;
+    }
+  }
+
+  //* Los cambios que se hagan en este control personalizado serán procesados a través de este 
+  //* método para que el FormControl de miFormulario.rating los capture
+  //* Registraremos en una variable la función de los cambios (changes)
+  //* Por detrás, angular trabaja con el evento (change)
+  registerOnChange(fn: Function): void {
+    this._onChanged = fn;
+  }
+
+  //* Por detrás, angular trabajar con el evento (blur)
+  registerOnTouched(fn: Function): void {
+    this._onTouch = fn;
+  }
+  //*#region 
 
 }
