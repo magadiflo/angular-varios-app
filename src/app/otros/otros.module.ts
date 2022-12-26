532bf872edd1d518c02a42556d7eeeedb1994ca5
Martín <magadiflo@gmail.com>
2022-12-26 08:16:22 -0500

    Reactive Forms Anidados (Nested Forms) y Control Value Accessor
    Componentes de Angular personaliazdos como FormControl Anidados

--- src/app/otros/otros.module.ts
+++ src/app/otros/otros.module.ts
@@ -6,13 +6,15 @@ import { OtrosRoutingModule } from './otros-routing.module';
 import { DataBindingComponent } from './data-binding/data-binding.component';
 import { ReactiveFormsComponent } from './reactive-forms/reactive-forms.component';
 import { StarRatingComponent } from './reactive-forms/star-rating/star-rating.component';
+import { RatingNameComponent } from './reactive-forms/rating-name/rating-name.component';
 
 
 @NgModule({
   declarations: [
     DataBindingComponent,
     ReactiveFormsComponent,
-    StarRatingComponent
+    StarRatingComponent,
+    RatingNameComponent
   ],
   imports: [
     CommonModule,
---
+++ src/app/otros/reactive-forms/rating-name/rating-name.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-rating-name',
+  templateUrl: './rating-name.component.html',
+  styleUrls: ['./rating-name.component.css']
+})
+export class RatingNameComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
